9ba3bdc1be605b519a24e42cf00b829e22e15abf
george <g.e.c.burton@gmail.com>
2020-03-25 11:50:36 +0000

    fixed example urls

--- example_project/example_app/urls.py
+++ example_project/example_app/urls.py
@@ -1,14 +1,14 @@
+"""this is just a placeholder, will be overwritten by:
 
+`$ python manage.py jsonschema2dj example_app
+
+"""
 from django.urls import path, include
 from rest_framework import routers
 
-from . import views
 
 router = routers.DefaultRouter()
 
-for name, obj in vars(views).items():
-    if isinstance(obj, type):
-        router.register(name, obj)
 
 urlpatterns = [
     path("", include(router.urls)),
