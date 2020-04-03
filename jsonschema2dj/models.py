8959be59f41e1603f3298cc3ac9aa2738fe16a7a
gecBurton <g.e.c.burton@gmail.com>
2020-04-03 19:44:10 +0100

    * removed venv! * added func to order models by dependency

--- jsonschema2dj/models.py
+++ jsonschema2dj/models.py
@@ -14,3 +14,25 @@ def build_model(name, sch):
         for field_name, field_sch in properties.items() if field_sch.get("type") in ("object", "array")
     }
     return name, fields, relations
+
+
+def build_dependency_order(schema):
+    dependency_order = []
+
+    def _get_dependencies(model_name):
+        model = schema["definitions"][model_name]
+        for field_name, field in model.get("properties", {}).items():
+            if field.get("type") == "object" and "$ref" in field:
+                _model_name = field["$ref"].split("/")[-1]
+                if model_name not in dependency_order:
+                    dependency_order.append(_model_name)
+                    _get_dependencies(_model_name)
+
+    for name in schema["definitions"]:
+        _get_dependencies(name)
+
+    for name in schema["definitions"]:
+        if name not in dependency_order:
+            dependency_order.append(name)
+
+    return dependency_order
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,13 @@ from json import load
 
 import pytest
 
-from jsonschema2dj.models import build_model
+from jsonschema2dj.models import build_model, build_dependency_order
 
 with open("tests/schemas/basic_model.json") as f:
     basic_model = load(f)
+
+with open("tests/schemas/simple_tree.json") as f:
+    simple_tree = load(f)
 
 results = {
     "person": (
@@ -34,3 +37,7 @@ results = {
 def test_build_model_pass(name, result):
     schema = basic_model["definitions"][name]
     assert build_model(name, schema), {} == result
+
+
+def test_build_dependency_order():
+    assert build_dependency_order(simple_tree) == ['F', 'D', 'C', 'E', 'B', 'A']
